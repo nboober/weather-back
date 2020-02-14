37306f9c1d68ccc0d570c77c0d7047b86e548503
Nick Boober <nboober@terpmail.umd.edu>
2020-02-14 14:47:41 -0500

    finished the search and geolocation methods. Also updated the test method

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -2,9 +2,35 @@ class WeatherController < ApplicationController
 
     def test
 
-        response = RestClient.get("https://www.metaweather.com/api/location/search/?query=san")
+        response = RestClient.get("https://www.metaweather.com/api/location/2487956/")
 
         render json: response
+    end
+
+    def search
+    
+        query = ["term"]
+
+        locate = RestClient.get("https://www.metaweather.com/api/location/search/?query=#{query}")
+
+        jsonResponse = JSON.parse(locate.body)
+        locationId = jsonResponse.first["woeid"]
+
+        response = RestClient.get("https://www.metaweather.com/api/location/#{locationId}/")
+
+        render json: response
+    
+    end
+
+    def geolocation
+        location = params["coordinates"]
+        response = RestClient.get("https://www.metaweather.com/api/location/search/?lattlong=#{location}")
+        jsonResponse = JSON.parse(response.body)
+        locationId = jsonResponse.first["woeid"]
+
+        response = RestClient.get("https://www.metaweather.com/api/location/#{locationId}/")
+
+        render json: response 
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get '/test', to: 'weather#test'
+  post '/search', to: 'weather#search'
+  post '/myarea', to: 'weather#geolocation'
 
 end

1f67debc1fb56d7bbbdcd4679846b58257c9bfa9
Nick Boober <nboober@terpmail.umd.edu>
2020-02-17 15:05:16 -0500

    added comments to the functions in weather. The meta weather API is restricted to just a few locations

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -6,24 +6,27 @@ class WeatherController < ApplicationController
         response = RestClient.get("https://www.metaweather.com/api/location/2487956/")
 
         render json: response
+        
     end
 
     def search
+
+    # The Search feature only returns certian locations because the API is restricted to just a few locations
+    query = params["term"]
     
-        query = params["term"]
+    locate = RestClient.get("https://www.metaweather.com/api/location/search/?query=#{query}")
+    jsonResponse = JSON.parse(locate.body)
+    
+    locationId = jsonResponse.first["woeid"]
+    response = RestClient.get("https://www.metaweather.com/api/location/#{locationId}/")
 
-        locate = RestClient.get("https://www.metaweather.com/api/location/search/?query=#{query}")
-        
-        jsonResponse = JSON.parse(locate.body)
-        locationId = jsonResponse.first["woeid"]
-
-        response = RestClient.get("https://www.metaweather.com/api/location/#{locationId}/")
-
-        render json: response
+    render json: response
     
     end
 
     def geolocation
+
+        # The Search feature only returns certian locations because the API is restricted to just a few locations
         location = params["coordinates"]
         response = RestClient.get("https://www.metaweather.com/api/location/search/?lattlong=#{location}")
         jsonResponse = JSON.parse(response.body)
